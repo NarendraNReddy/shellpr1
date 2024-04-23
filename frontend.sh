6ab8252ce74b1f14c827f0006e20ea7572c46a0e
NarendraReddy <narendra.nya@gmail.com>
2024-04-23 16:50:04 -0700

    script calling

--- frontend.sh
+++ frontend.sh
@@ -4,29 +4,29 @@ check_root
 
 
 dnf install nginx -y &>>$LOGFILE
-VALIDATE $? "nginx install"
+#VALIDATE $? "nginx install"
 
 
 systemctl enable nginx &>>$LOGFILE
-VALIDATE $? "enable nginx "
+#VALIDATE $? "enable nginx "
 
 systemctl start nginx &>>$LOGFILE
-VALIDATE $? "start nginx"
+#VALIDATE $? "start nginx"
 
 rm -rf /usr/share/nginx/html/* &>>$LOGFILE
-VALIDATE $? "remove nginx hmtl"
+#VALIDATE $? "remove nginx hmtl"
 
 curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
-VALIDATE $? "download frontend code"
+#VALIDATE $? "download frontend code"
 
 cd /usr/share/nginx/html &>>$LOGFILE
-VALIDATE $? "Entering into nginx html folder"
+#VALIDATE $? "Entering into nginx html folder"
 
 unzip /tmp/frontend.zip &>>$LOGFILE
-VALIDATE $? "Copy the frontend code"
+#VALIDATE $? "Copy the frontend code"
 
 cp -rf /home/ec2-user/shellpr1/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
-VALIDATE $? "copying expense.conf"
+#VALIDATE $? "copying expense.conf"
 
 systemctl restart nginx &>>$LOGFILE
-VALIDATE $? "Restart nginx"
+#VALIDATE $? "Restart nginx"
