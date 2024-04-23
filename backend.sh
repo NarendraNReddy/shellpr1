fe84ab2433256734d3218a7a87f5d940657ac196
NarendraReddy <narendra.nya@gmail.com>
2024-04-23 16:02:21 -0700

    script calling

--- backend.sh
+++ backend.sh
@@ -1,37 +1,12 @@
 #!/bin/bash
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-BL="\e[34m"
-N="\e[0m"
+source ./common.sh
+check_root
 
 echo "Enter password:"
 read DB_SERVER_PASSWORD
 #ExpenseApp@1
 
-VALIDATE()
-{
-    if [ $1 -ne 0 ];
-    then 
-        echo -e "$2 is ... $R FAILURE $N"
-    else 
-        echo -e "$2 is ... $G SUCCESS $N"    
-    fi
-}
-
-USERID=$(id -u)
-
-if [ $USERID -ne 0 ];
-then 
-    echo "Be a super user to install the commands"
-    exit 1
-else 
-    echo "Super User"     
-fi
 
 
 
---
+++ common.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+BL="\e[34m"
+N="\e[0m"
+
+echo "Enter password:"
+read DB_SERVER_PASSWORD
+#ExpenseApp@1
+
+VALIDATE()
+{
+    if [ $1 -ne 0 ];
+    then 
+        echo -e "$2 is ... $R FAILURE $N"
+    else 
+        echo -e "$2 is ... $G SUCCESS $N"    
+    fi
+}
+
+
+check_root()
+{
+   USERID=$(id -u)
+
+    if [ $USERID -ne 0 ];
+    then 
+        echo "Be a super user to install the commands"
+        exit 1
+    else 
+        echo "Super User"     
+    fi
+}  
