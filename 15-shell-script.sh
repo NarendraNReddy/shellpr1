db32ac55f0703a992c2ba88f32bb281f183afb83
NarendraReddy <narendra.nya@gmail.com>
2024-04-23 15:17:52 -0700

    script calling

---
+++ 15-shell-script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+COURSE="Devops with current script"
+
+echo "Before calling other script,the variable course:$COURSE"
+echo "Pid of the current script:$$"
+
+#./16-other-script.sh
+
+echo "After calling other script,the variable course:$COURSE"
---
+++ 16-other-script.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+COURSE="Devops with other script"
+echo "other script,the variable course:$COURSE"
+echo "Pid of the current script:$$"
+
