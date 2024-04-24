5a20b46105aeeeb8a3788b85df1ba54eecf062f9
NarendraReddy <narendra.nya@gmail.com>
2024-04-24 06:34:03 -0700

    du

--- 20-diskusage.sh
+++ 20-diskusage.sh
@@ -15,5 +15,7 @@ do
 
 done <<< $DISK_USAGE
 
-echo "$MESSAGE" |  mail -s "disk usage alert" narendra.h1b@gmail.com
+echo "This is a test mail & Date $(date)" | mail -s "message" narendra.h1b@gmail.com
+
+$echo "$MESSAGE" |  mail -s "disk usage alert" narendra.h1b@gmail.com
 
