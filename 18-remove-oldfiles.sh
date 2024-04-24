5e15d5a0b9c68651ba6c484985d54ddfe60ac0de
NarendraReddy <narendra.nya@gmail.com>
2024-04-23 18:09:18 -0700

    script calling

--- 18-remove-oldfiles.sh
+++ 18-remove-oldfiles.sh
@@ -1,30 +1,12 @@
 #!/bin/bash
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-BL="\e[34m"
-N="\e[0m"
-
 SOURCE_DIRECTORY=/tmp/app-logs
-
 
 if [ -d $SOURCE_DIRECTORY ];
 then 
     echo "Folder exists"
 else 
-    echo "Folder does not exists.Please create a folder" 
-    exit 1   
-fi 
-
-FILES=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
-
-#echo $FILES
-
-
-while IFS= read -r line
-do 
-    rm -rf $line
-done <<< $FILES 
+    echo "Please create a folder:$SOURCE_DIRECTORY"    
+fi
 
 
