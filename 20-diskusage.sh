02914ce4cc5ab8555df5d02b471423447669b5ea
NarendraReddy <narendra.nya@gmail.com>
2024-04-24 05:44:39 -0700

    du

--- 20-diskusage.sh
+++ 20-diskusage.sh
@@ -5,7 +5,7 @@ DISK_THRESHOLD=75
 
 while IFS= read -r line 
 do 
-    USAGE=$(echo $line | awk -F " " '{print $6F}')
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
     FOLDER=$(echo $line | awk -F " " '{print $NF}')
     echo $USAGE
     #echo $FOLDER
