ce7c254a828e0316edd4c72960b5b4b2be7e7a2a
NarendraReddy <narendra.nya@gmail.com>
2024-04-24 04:38:11 -0700

    Modify

--- 18-remove-oldfiles.sh
+++ 18-remove-oldfiles.sh
@@ -9,13 +9,11 @@ else
     echo "Please create a folder:$SOURCE_DIRECTORY"    
 fi
 
-
-
-
 FILES=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
 
 while IFS= read -r line
 do 
     rm -rf $line
 done <<< $FILES
+
 
